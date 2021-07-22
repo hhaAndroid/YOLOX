901ba1a7dc75d6913e87b045a4dc871111f3ca3a
PJLAB\huanghaian <1286304229@qq.com>
2021-07-22 14:39:55 +0800

    modify data_dir

--- yolox/exp/yolox_base.py
+++ yolox/exp/yolox_base.py
@@ -25,10 +25,12 @@ class Exp(BaseExp):
         # ---------------- dataloader config ---------------- #
         # set worker to 4 for shorter dataloader init time
         self.data_num_workers = 4
+        # self.data_num_workers = 0
         self.input_size = (640, 640)
         self.random_size = (14, 26)
         self.train_ann = "instances_train2017.json"
         self.val_ann = "instances_val2017.json"
+        self.data_dir = '/mnt/data1/huanghaian/coco'
 
         # --------------- transform config ----------------- #
         self.degrees = 10.0
@@ -90,7 +92,7 @@ class Exp(BaseExp):
         )
 
         dataset = COCODataset(
-            data_dir=None,
+            data_dir=self.data_dir,
             json_file=self.train_ann,
             img_size=self.input_size,
             preproc=TrainTransform(
@@ -205,7 +207,7 @@ class Exp(BaseExp):
         from yolox.data import COCODataset, ValTransform
 
         valdataset = COCODataset(
-            data_dir=None,
+            data_dir=self.data_dir,
             json_file=self.val_ann if not testdev else "image_info_test-dev2017.json",
             name="val2017" if not testdev else "test2017",
             img_size=self.test_size,
